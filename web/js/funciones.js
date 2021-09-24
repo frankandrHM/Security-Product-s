7b8f16f2dae4ba8551e38d81ad5dc32d4239c0b5
frankandrHM <91352395+frankandrHM@users.noreply.github.com>
2021-09-24 18:37:15 -0500

    Add files via upload

---
+++ web/js/funciones.js
@@ -0,0 +1,56 @@
+/* global swal */
+
+$(document).ready(function () {
+    $("tr #deleteItem").click(function (e) {
+        e.preventDefault();
+        var idp = $(this).parent().find('#item2').val();        
+        swal({
+            title: "Esta Seguro de Eliminar?",
+            text: "Una una Vez Eliminado, Debera Agregar de Nuevo!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true
+        }).then((willDelete) => {
+            if (willDelete) {
+                eliminar(idp);
+                swal(" The record was deleted! ", {
+                    icon: "success",
+                }).then((willDelete) => {
+                    if (willDelete) {
+                        parent.location.href = "Controlador?accion=carrito";
+                    }
+                });
+            }
+        });
+    });
+    function eliminar(idp) {
+        var url = "Controlador?accion=deleteProducto&id=" + idp;
+        console.log("hol");
+        $.ajax({
+            type: 'POST',
+            url: url,
+            async: true,
+            success: function (r) {
+            }
+        });
+    }
+
+    $("tr #Cant").click(function (e) {
+        var idp = $(this).parent().find('#item1').val();
+        var cantidad = $(this).parent().find('#Cant').val();
+        var url = "Controlador?accion=updateCantidad";
+        console.log(idp, cantidad);
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: "id=" + idp + "&cantidad=" + cantidad,
+            success: function (data, textStatus, jqXHR) {
+//                parent.location.href = "Controlador?accion=carrito";
+            }
+        });
+    });    
+   
+});
+
+
+
